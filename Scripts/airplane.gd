8b346abbb73c1cd98e3e7080f811c0b43d7a1cbf
Edouard Lambert <edouard.lambert@student.junia.com>
2026-03-03 23:18:26 +0100

    New plane

--- Scripts/airplane.gd
+++ Scripts/airplane.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("wingRoot|wingRootAction_001")
+	$AnimationPlayer.play("animation_biplane_new")
 	pass # Replace with function body.
 
 
